1ec3d44a9cae3fb69021080a73ea9751df26f88f
Clever Edu Perea <55072929+kleber89@users.noreply.github.com>
2022-10-26 00:42:15 -0500

    Creación Spring 2

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Agro Insumos Colombia</h1>
+        <p>Tu tienda especializada en insumos agropecuarios</p>
+      </header>
+      <nav className="navbar">
+          <a className="enlacenav" href='#'>Inicio</a>
+          
+          <a className="enlacenav" href='#'>¿Quienes Somos?</a>
+          
+          <a className="enlacenav" href='#'>Productos</a>
+         
+          <a className="enlacenav" href='#'>Carrito</a>
+        </nav>
+    </div>
+  );
+}
+
+export default App;
